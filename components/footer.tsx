ec6af28621551b98467ff249d7be80594969df8c
Kaitlyn Brandt <kaitlyn.brandt@proton.me>
2024-08-20 17:37:21 -0500

    Update footer.tsx
    The link was to 'savedeadings' and needed a user id

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,31 +1,31 @@
 'use client'
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
-export default function Footer(){
+export default function Footer() {
   const { data: session, status } = useSession();
-    return (    
-  
-        <Navbar>
-            <Container>
-         
-              <Navbar.Brand >
-              
-         
-      
-              </Navbar.Brand>
-              {status === 'authenticated' ? <h1>Hi, {session.user?.name}! <Link href={`/savedeadings`}>View your saved readings?</Link></h1> :  <Link href="/api/auth/signin">Sign in</Link> }
-      
-      
-              <Navbar.Toggle />
-              <Navbar.Collapse className="justify-content-end">
-                
-            
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-      
-        )
+  return (
+
+    <Navbar>
+      <Container>
+
+        <Navbar.Brand >
+
+
+
+        </Navbar.Brand>
+        {status === 'authenticated' ? <h1>Hi, {session.user?.name}! <Link href={`/savedreadings/${session.user?.id}`}>View your saved readings?</Link></h1> : <Link href="/api/auth/signin">Sign in</Link>}
+
+
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  )
 }
