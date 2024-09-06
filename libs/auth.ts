59924bb4f8a399f846b1edd96939539d4012083b
Kaitlyn Brandt <kaitlyn.brandt@proton.me>
2024-09-05 23:54:01 -0500

    Update auth.ts

--- libs/auth.ts
+++ libs/auth.ts
@@ -31,9 +31,9 @@ export const authOptions: NextAuthOptions = {
     },
     async redirect({ url, baseUrl }) {
       // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}`
+      if (url.startsWith("/")) return `${baseUrl}${url}`
       // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return baseUrl
+      else if (new URL(url).origin === baseUrl) return url
       return baseUrl
     }
   },
